15047845295b524281f86feea8b950edb62a7d37
judahwon <hwondaa@gmail.com>
2025-06-23 23:19:52 +0900

    style: font 적용한다

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        arita: ['var(--font-arita)'],
+        pretendard: ['var(--font-pretendard)'],
+        apple: ['var(--font-apple)'],
+        noto: ['var(--font-noto)'],
+      },
+    },
   },
   plugins: [],
-} 
+};
