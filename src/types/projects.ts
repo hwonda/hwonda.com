d7336a94994ed1072ca790c96b85d6fa891988e7
judahwon <hwondaa@gmail.com>
2025-07-15 23:37:54 +0900

    feat: project 타입 및 레이아웃 수정한다

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,14 +1,34 @@
-export interface Project {
-  id: number;
-  title: string;
-  imageUrl: string;
-  description?: string;
-  tags?: string[];
-  projectId?: string;
-  subtitle?: string;
-}
-
 export interface YearProjects {
   year: string;
   projects: Project[];
+}
+
+export interface Project {
+  id: string;
+  title: string;
+  description: {
+    short: string;
+    full: string;
+  };
+  images: {
+    thumbnail: string;
+    full?: string[];
+    video?: string;
+  };
+  urls?: {
+    github?: string;
+    demo?: string;
+    etc?: string;
+  };
+  points?: ProjectPoints[];
+  period?: string;
+  role?: string;
+  team?: string;
+  techStack?: string[];
+}
+
+interface ProjectPoints {
+  challenges?: string;
+  solutions?: string;
+  results?: string;
 }
