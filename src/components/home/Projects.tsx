27331c5dea51f96e890541ae8121b1e443adf671
judahwon <hwondaa@gmail.com>
2025-07-20 23:27:08 +0900

    fix: 프로젝트 이미지 표출 로직 변경한다

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   useEffect(() => {
     const options = {
       root: null,
-      rootMargin: '-10% 0px',
+      rootMargin: '0px',
       threshold: 0.1,
     };
 
@@ -27,7 +27,8 @@ export default function Projects() {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           const target = entry.target as HTMLElement;
-          const delay = parseInt(target.dataset.index || '0') * 100 + 300; // 제목 이후에 시작하도록 300ms 추가
+          const projectIndex = parseInt(target.dataset.index || '0');
+          const delay = projectIndex * 100;
           target.style.animationDelay = `${delay}ms`;
           target.classList.add('animate-intro');
           observerProjects.unobserve(target);
@@ -59,7 +60,7 @@ export default function Projects() {
         <h2 className="font-paperlogy mb-12 text-3xl font-bold md:text-4xl">
           프로젝트
         </h2>
-        <div className="grid grid-cols-1 gap-10 space-y-16 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-10 space-y-16">
           {projectsData.map((yearData, yearIndex) => {
             // 각 연도별 프로젝트 배열 초기화
             projectRefs.current[yearIndex] =
@@ -75,11 +76,8 @@ export default function Projects() {
                 >
                   {yearData.year} ({yearData.projects.length})
                 </h3>
-                <div className="grid grid-cols-2 gap-1.5">
+                <div className="grid grid-cols-1 space-y-4 space-x-1.5 sm:grid-cols-2 sm:space-y-6 md:space-y-8 lg:space-y-10">
                   {yearData.projects.map((project, projectIndex) => {
-                    const globalIndex =
-                      yearData.projects.length * yearIndex + projectIndex;
-
                     return (
                       <a
                         ref={(el) => {
@@ -87,18 +85,21 @@ export default function Projects() {
                         }}
                         href={`/projects/${project.id}`}
                         key={project.id}
-                        data-index={globalIndex}
-                        className="group bg-gray-9 border-gray8 relative aspect-video rounded-lg border opacity-0 shadow-sm transition-all hover:cursor-pointer hover:shadow-md"
+                        data-index={projectIndex}
+                        className="group border-background hover:border-accent-2 relative flex aspect-video flex-col gap-1 rounded-lg border p-1 opacity-0 shadow-sm transition-all duration-300 hover:cursor-pointer hover:shadow-md"
                       >
                         <img
                           src={project.images.thumbnail}
                           alt={project.title}
-                          className="h-full w-full rounded-lg object-cover"
+                          className="h-full w-full rounded-lg object-cover opacity-85 transition-all duration-300 group-hover:p-1 group-hover:opacity-100"
                         />
-                        <div className="bg-opacity-50 absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
-                          <h4 className="font-pretendard px-4 text-center text-lg font-bold text-white">
+                        <div className="flex flex-col gap-1 p-1">
+                          <h4 className="font-pretendard text-gray1 group-hover:text-accent-1 text-base font-bold lg:text-lg">
                             {project.title}
                           </h4>
+                          <div className="text-gray1 group-hover:text-main text-sm">
+                            {project.subtitle}
+                          </div>
                         </div>
                       </a>
                     );
