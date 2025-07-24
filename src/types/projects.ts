bb791bf899de9627c0e0f70162045a300b6b3ff0
judahwon <hwondaa@gmail.com>
2025-07-25 00:11:43 +0900

    refact: project etc url 타입 추가한다

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -3,34 +3,43 @@ export interface YearProjects {
   projects: Project[];
 }
 
-export interface Project {
-  id: string;
+interface Description {
+  short: string;
+  full: string;
+}
+
+interface Images {
+  thumbnail: string;
+  full?: string[];
+  video?: string;
+  captions?: string[]; // 각 이미지에 대한 설명 추가
+}
+
+interface EtcUrls {
   title: string;
-  subtitle?: string;
-  description: {
-    short: string;
-    full: string;
-  };
-  images: {
-    thumbnail: string;
-    full?: string[];
-    video?: string;
-    captions?: string[]; // 각 이미지에 대한 설명 추가
-  };
-  urls?: {
-    github?: string;
-    demo?: string;
-    etc?: string;
-  };
-  points?: ProjectPoints[];
-  period?: string;
-  role?: string;
-  team?: string;
-  techStack?: string[];
+  url: string;
+}
+interface Urls {
+  github?: string;
+  demo?: string;
+  etc?: EtcUrls[];
 }
 
 interface ProjectPoints {
   challenges?: string;
   solutions?: string;
   results?: string;
+}
+export interface Project {
+  id: string;
+  title: string;
+  subtitle?: string;
+  description: Description;
+  images: Images;
+  urls?: Urls;
+  points?: ProjectPoints[];
+  period?: string;
+  role?: string;
+  team?: string;
+  techStack?: string[];
 }
