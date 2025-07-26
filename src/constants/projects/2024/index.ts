56b92c535aca52bfb4d5fc07ed3eed833d64aa78
judahwon <hwondaa@gmail.com>
2025-07-26 13:55:33 +0900

    fix: aqua-farm-2024 제거한다

--- src/constants/projects/2024/index.ts
+++ src/constants/projects/2024/index.ts
@@ -1,8 +1,7 @@
 import { aisar } from './aisar';
-import { aquaFarm2024 } from './aquaFarm2024';
 import { mmis2024 } from './mmis2024';
 import { nims } from './nims';
 
-export const projects2024 = [nims, aisar, mmis2024, aquaFarm2024];
+export const projects2024 = [nims, aisar, mmis2024];
 
-export { aisar, aquaFarm2024, mmis2024, nims };
+export { aisar, mmis2024, nims };
