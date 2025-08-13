d171925c0b5870c7a3f3c7b8a7807e80b1ea6d13
hwonda <hwondaa@gmail.com>
2025-08-13 17:52:00 +0900

    docs: 2024 프로젝트 일부 삭제한다

--- src/constants/projects/2025/index.ts
+++ src/constants/projects/2025/index.ts
@@ -1,8 +1,7 @@
 import { aquaFarm2025 } from './aquaFarm2025';
 import { digitalTwin } from './digitalTwin';
 import { mmis2025 } from './mmis2025';
-import { narae } from './narae';
 
-export const projects2025 = [mmis2025, aquaFarm2025, digitalTwin, narae];
+export const projects2025 = [mmis2025, aquaFarm2025, digitalTwin];
 
-export { aquaFarm2025, digitalTwin, mmis2025, narae };
+export { aquaFarm2025, digitalTwin, mmis2025 };
