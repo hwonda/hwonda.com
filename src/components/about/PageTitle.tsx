20c1c3feada00248847edb12952c0b52b3ac027f
judahwon <hwondaa@gmail.com>
2025-09-06 16:41:47 +0900

    feat: about 페이지 내 이력서 다운로드 버튼 추가한다

--- src/components/about/PageTitle.tsx
+++ src/components/about/PageTitle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Download } from 'lucide-react';
 
 interface PageTitleProps {
   name: string;
@@ -10,12 +10,22 @@ export default function PageTitle({
   suffix = '에 대하여',
 }: PageTitleProps) {
   return (
-    <h1 className="font-pretendard relative mb-0 text-4xl font-bold sm:mb-12 md:text-5xl">
-      <span className="from-accent-3 to-accent-1 bg-gradient-to-b bg-clip-text text-transparent">
-        {name}
-      </span>
-      {suffix}
-      <div className="from-accent-1 to-accent-3 absolute bottom-[-16px] left-0 h-[12px] w-[102px] rounded-full bg-gradient-to-b opacity-30 md:w-[132px]"></div>
-    </h1>
+    <div className="flex justify-between">
+      <h1 className="font-pretendard relative mb-0 text-4xl font-bold sm:mb-12 md:text-5xl">
+        <span className="from-accent-3 to-accent-1 bg-gradient-to-b bg-clip-text text-transparent">
+          {name}
+        </span>
+        {suffix}
+        <div className="from-accent-1 to-accent-3 absolute bottom-[-16px] left-0 h-[12px] w-[102px] rounded-full bg-gradient-to-b opacity-30 md:w-[132px]"></div>
+      </h1>
+      <a
+        href="/resume_dahwon_ju.pdf"
+        download
+        className="text-main hover:text-accent-1 flex items-center gap-2 pb-6 font-medium transition-colors duration-300"
+      >
+        <Download className="h-4 w-4" />
+        이력서(pdf)
+      </a>
+    </div>
   );
 }
