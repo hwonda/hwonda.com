40b919da55cabbf34ecffec304935a71ce076089
judahwon <hwondaa@gmail.com>
2025-09-07 23:45:03 +0900

    chore: canonical tags 추가한다

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hwonda.com',
+  site: 'https://www.hwonda.com',
   integrations: [
     react(),
     sitemap({
-      filter: (page) => !page.includes('/admin/'),
+      filter: (page) => !page.includes('/admin/') && !page.includes('/404'),
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
