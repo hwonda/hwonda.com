b646cf8564e3e5de22df54739eefc1c14180a868
judahwon <hwondaa@gmail.com>
2026-03-01 22:16:51 +0900

    fix: koast-ui 2025 프로젝트로 이동한다

--- src/constants/projects/2025/index.ts
+++ src/constants/projects/2025/index.ts
@@ -1,14 +1,15 @@
 import { aquaFarm2025 } from './aquaFarm2025';
 import { digitalTwin } from './digitalTwin';
+import { koastUi } from './koastUi';
 import { mmis2025 } from './mmis2025';
 
 // 공통 (기존 호환성 유지)
-export const projects2025 = [mmis2025, aquaFarm2025, digitalTwin];
+export const projects2025 = [mmis2025, aquaFarm2025, digitalTwin, koastUi];
 
 // FE용 2025 프로젝트
-export const feProjects2025 = [mmis2025, aquaFarm2025, digitalTwin];
+export const feProjects2025 = [mmis2025, aquaFarm2025, digitalTwin, koastUi];
 
 // TPM용 2025 프로젝트
-export const tpmProjects2025 = [mmis2025, aquaFarm2025, digitalTwin];
+export const tpmProjects2025 = [mmis2025, aquaFarm2025, digitalTwin, koastUi];
 
-export { aquaFarm2025, digitalTwin, mmis2025 };
+export { aquaFarm2025, digitalTwin, koastUi, mmis2025 };
--- src/constants/projects/side/index.ts
+++ src/constants/projects/side/index.ts
@@ -1,16 +1,15 @@
 import { blog } from './blog';
 import { diki } from './diki';
-import { koastUi } from './koastUi';
 import { reactMultiEmail } from './reactMultiEmail';
 import { zizizip } from './zizizip';
 
 // 공통 (기존 호환성 유지)
-export const sideProjects = [diki, zizizip, koastUi, blog, reactMultiEmail];
+export const sideProjects = [diki, zizizip, blog, reactMultiEmail];
 
 // FE용 사이드 프로젝트
-export const feSideProjects = [diki, zizizip, koastUi, blog, reactMultiEmail];
+export const feSideProjects = [diki, zizizip, blog, reactMultiEmail];
 
 // TPM용 사이드 프로젝트
-export const tpmSideProjects = [diki, zizizip, koastUi, blog, reactMultiEmail];
+export const tpmSideProjects = [diki, zizizip, blog, reactMultiEmail];
 
-export { blog, diki, koastUi, reactMultiEmail, zizizip };
+export { blog, diki, reactMultiEmail, zizizip };
