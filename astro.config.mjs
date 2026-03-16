a840fbe5e1d9f667da1c54e667806f2e385e3fe6
judahwon <hwondaa@gmail.com>
2026-03-16 13:19:04 +0900

    fix: 진입점을 /fe로 수정 후 index.astro 아카이빙한다

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.hwonda.com',
+  redirects: {
+    '/': '/fe',
+  },
   integrations: [
     react(),
     sitemap({
